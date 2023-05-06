a0382868ab9f601c748284d36a82e0bca22df5ba
Bruce MacDonald <bruce@bkmacd.com>
2023-05-07 00:38:07 +0100

    tz convert to C++20

--- include/timestamp.convert.hpp
+++ include/timestamp.convert.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <unistd.h>
 #include <sys/stat.h>
 #include <chrono>
 #include <iomanip>
 #include <fstream>
 #include <optional>
 
-#include <cctz/time_zone.h>
 
 /**
  * pod field converter for storing a chrono timestamp in a capnp uint64 as nanoseconds.
@@ -15,41 +13,7 @@
 
 namespace podgen {
 
-using timestamp_clock = std::chrono::system_clock;
-using Timestamp = std::chrono::time_point<timestamp_clock, std::chrono::nanoseconds>;
-
-/// Some ugliness to find the system time zone.
-/// It's either the contents of /etc/timezone, or a symlink from /etc/timezone into /usr/share/zoneinfo.
-inline cctz::time_zone getSystemTimeZone() {
-    static const char* tzfile = "/etc/timezone";
-    cctz::time_zone tz;
-    struct stat st;
-
-    if (lstat(tzfile, &st) == 0 && S_ISLNK(st.st_mode)) {
-        char buf[64];
-        auto n = readlink(tzfile, buf, sizeof(buf));
-
-        bool foundOne = false;
-        for (auto i = n - 1; i >= 0; i--) {
-            if (buf[i] == '/') {
-                if (foundOne) {
-                    cctz::load_time_zone(&buf[i+1], &tz);
-                } else {
-                    foundOne = true;
-                }
-            }
-        }
-    } else {
-        std::ifstream is("/etc/timezone");
-        if (is.good()) {
-            std::string tzname;
-            is >> std::ws >> tzname;
-            cctz::load_time_zone(tzname, &tz);
-        }
-    }
-
-    return tz;
-}
+using Timestamp = std::chrono::sys_time<std::chrono::nanoseconds>;
 
 inline Timestamp timestampFromNanos(uint64_t value) {
     return Timestamp(std::chrono::nanoseconds(value));
@@ -60,17 +24,9 @@ inline uint64_t timestampToNanos(const Timestamp& timestamp) {
 }
 
 inline void outTimestamp(std::ostream& os, const Timestamp& o) {
-    static const auto tz = getSystemTimeZone();
-
-    os << '[';
-    os << cctz::format("%Y-%m-%d %H:%M:%S", o, tz);
-
-    auto nanos = timestampToNanos(o);
-    static const uint64_t NS_PER_SEC = 1000000000L;
-    auto ns = nanos % NS_PER_SEC;
-
-    std::string us = "000000" + std::to_string(ns / 1000);
-    os << "." << us.substr(us.size() - 6) << cctz::format(" %z", o, tz) << ']';
+    static const auto tz = std::chrono::current_zone();
+    const std::chrono::zoned_time zt{tz, o};
+    os << '[' << zt << ']';;
 }
 
 /// Include an std::optional output function in case the field is declared as an optional.
@@ -83,7 +39,7 @@ inline void outTimestamp(std::ostream& os, const std::optional<Timestamp>& o) {
     }
 }
 
-}
+} // namespace podgen
 
 
 namespace std {
@@ -94,4 +50,4 @@ template <> struct hash<podgen::Timestamp> {
     }
 };
 
-}
+} // namespace std
